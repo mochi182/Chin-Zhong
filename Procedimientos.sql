44b4456deca443e6b69b89b81d4855ca3447cbb6
mochi182 <mochi182tv@gmail.com>
2020-07-09 12:24:48 -0500

    Primer commit

---
+++ Procedimientos.sql
@@ -0,0 +1,179 @@
+/* ------------------------- DDL ------------------------- */
+
+/* --------------- Nivel 1 --------------- */
+
+CREATE OR REPLACE PROCEDURE insertar_sucursal(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_direccion sucursal.direccion%TYPE,
+    p_telefono sucursal.telefono%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO sucursal(id_sucursal, direccion, telefono)
+        VALUES (secuencia_id_sucursal.nextval, p_direccion, p_telefono);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_sucursal;
+    /
+
+CREATE OR REPLACE PROCEDURE insertar_proveedor(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_nombre proveedor.nombre%TYPE,
+    p_direccion proveedor.direccion%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO proveedor(id_proveedor, nombre, direccion)
+        VALUES (secuencia_id_proveedor.nextval, p_nombre, p_direccion);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_proveedor;
+    /
+
+CREATE OR REPLACE PROCEDURE insertar_categoria(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_abreviatura categoria.abreviatura%TYPE,
+    p_descripcion categoria.descripcion%TYPE,
+    p_nombre categoria.nombre%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO categoria(id_categoria, abreviatura, descripcion, nombre)
+        VALUES (secuencia_id_categoria.nextval, p_abreviatura, p_descripcion, p_nombre);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_categoria;
+    /
+
+
+CREATE OR REPLACE PROCEDURE insertar_bodega(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_ubicacion bodega.ubicacion%TYPE,
+    p_area bodega.area%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO bodega(id_bodega, ubicacion, area)
+        VALUES (secuencia_id_bodega.nextval, p_ubicacion, p_area);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_bodega;
+    /
+
+DECLARE
+    v_ubicacion bodega.ubicacion%TYPE := 'Ciudad de Panamá';
+    v_area bodega.area%TYPE := 2000;
+    mensaje VARCHAR2(50);
+BEGIN
+    insertar_bodega(v_ubicacion, v_area, mensaje);
+    DBMS_OUTPUT.PUT_LINE(mensaje);
+END;
+/
+
+/* --------------- Nivel 2 --------------- */
+
+CREATE OR REPLACE PROCEDURE insertar_articulo(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_nombre articulo.nombre%TYPE,
+    p_descripcion articulo.descripcion%TYPE,
+    p_costo articulo.costo%TYPE,
+    p_unidad_de_medida articulo.unidad_de_medida%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO articulo(id_articulo, nombre, descripcion, costo, unidad_de_medida)
+        VALUES (secuencia_id_articulo.nextval, p_nombre, p_descripcion, p_costo, p_unidad_de_medida);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_articulo;
+    /
+
+CREATE OR REPLACE PROCEDURE insertar_empleado(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_nombre empleado.nombre%TYPE,
+    p_apellido empleado.apellido%TYPE,
+    p_edad empleado.edad%TYPE,
+    p_ocupacion empleado.ocupacion%TYPE,
+    p_sexo empleado.sexo%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO empleado(id_empleado, nombre, edad, ocupacion, sexo)
+        VALUES (secuencia_id_empleado.nextval, p_nombre, p_edad, p_ocupacion, p_sexo);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_empleador;
+    /
+
+CREATE OR REPLACE PROCEDURE insertar_cliente(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_cedula cliente.cedula%TYPE,
+    p_nombre cliente.nombre%TYPE,
+    p_apellido cliente.apellido%TYPE,
+    p_sexo cliente.sexo%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO cliente(id_cliente, cedula, nombre, apellido, sexo)
+        VALUES (secuencia_id_cliente.nextval, p_cedula, p_nombre, p_apellido, p_sexo);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_cliente;
+    /
+
+/* --------------- Nivel 3 --------------- */
+
+CREATE OR REPLACE PROCEDURE insertar_abasto(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_id_sucursal sucursal.id_sucursal%TYPE,
+    p_id_bodega bodega.id_bodega%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO abasto(id_abasto, id_sucursal, id_bodega)
+        VALUES (secuencia_id_abasto.nextval, p_id_sucursal, p_id_bodega);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_abasto;
+    /
+
+CREATE OR REPLACE PROCEDURE insertar_pedido(
+    -- Procedimiento para ingresar en tabla "bodega".
+    p_id_sucursal sucursal.id_sucursal%TYPE,
+    p_id_cliente cliente.id_cliente%TYPE,
+    p_mensaje OUT VARCHAR2
+    ) AS
+    BEGIN
+        p_mensaje := 'Proceso ejecutado con éxito.';
+        INSERT INTO pedido(id_pedido, id_sucursal, id_cliente)
+        VALUES (secuencia_id_pedido.nextval, p_id_sucursal, p_id_cliente);
+        COMMIT;
+    EXCEPTION
+        WHEN others THEN
+            p_mensaje := 'Error desconocido';
+    END insertar_pedido;
+    /
---
+++ Tablas y restricciones.sql
@@ -0,0 +1,221 @@
+/* ------------------------- DDL ------------------------- */
+
+/* --------------- Nivel 1: Tablero --------------- */
+
+CREATE TABLE sucursal(
+    id_sucursal NUMBER NOT NULL,
+    direccion VARCHAR(50),
+    telefono NUMBER(8),
+    PRIMARY KEY(id_sucursal)
+);
+
+CREATE SEQUENCE secuencia_id_sucursal
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+
+CREATE TABLE proveedor(
+    id_proveedor NUMBER,
+    nombre VARCHAR(50),
+    direccion VARCHAR(50),
+    PRIMARY KEY(id_proveedor)
+);
+
+CREATE SEQUENCE secuencia_id_proveedor
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE categoria(
+    id_categoria  NUMBER NOT NULL,
+    abreviatura VARCHAR(30),
+    descripcion VARCHAR(100),
+    nombre VARCHAR(50),
+    PRIMARY KEY(id_categoria)
+);
+
+CREATE SEQUENCE secuencia_id_categoria
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE bodega(
+    id_bodega NUMBER NOT NULL,
+    ubicacion VARCHAR(50),
+    area NUMBER,
+    PRIMARY KEY(id_bodega)
+);
+
+CREATE SEQUENCE secuencia_id_bodega
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+/* --------------- Nivel 2: Fichas --------------- */
+
+CREATE TABLE articulo(
+    id_articulo NUMBER NOT NULL,
+    nombre VARCHAR(50) UNIQUE,
+    descripcion VARCHAR(100),
+    costo NUMBER(10,2),
+    unidad_de_medida VARCHAR(30),
+    PRIMARY KEY(id_articulo)
+);
+
+CREATE SEQUENCE secuencia_id_articulo
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE empleado(
+    id_empleado NUMBER NOT NULL,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    edad NUMBER,
+    ocupacion VARCHAR(50),
+    sexo VARCHAR(1),
+    PRIMARY KEY(id_empleado)
+);
+ALTER TABLE empleado ADD CHECK (sexo='M' OR sexo='F');
+
+CREATE SEQUENCE secuencia_id_empleado
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE cliente(
+    id_cliente NUMBER NOT NULL,
+    cedula VARCHAR(30),
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    sexo VARCHAR(1),
+    PRIMARY KEY(id_cliente)
+);
+ALTER TABLE cliente ADD CHECK (sexo='M' OR sexo='F');
+
+CREATE SEQUENCE secuencia_id_cliente
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+/* --------------- Nivel 3: Actividades --------------- */
+
+CREATE TABLE abasto(
+    id_abasto NUMBER NOT NULL,
+    id_sucursal NUMBER, --FK
+    id_bodega NUMBER, --FK
+    PRIMARY KEY(id_abasto)
+);
+ALTER TABLE abasto ADD FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal);
+ALTER TABLE abasto ADD FOREIGN KEY (id_bodega) REFERENCES bodega(id_bodega);
+
+CREATE SEQUENCE secuencia_id_abasto
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE pedido(
+    id_pedido NUMBER NOT NULL,
+    id_sucursal NUMBER, --FK
+    id_cliente NUMBER, --FK
+    PRIMARY KEY(id_pedido)
+);
+ALTER TABLE pedido ADD FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal);
+ALTER TABLE pedido ADD FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+
+CREATE SEQUENCE secuencia_id_pedido
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+CREATE TABLE venta(
+    id_venta NUMBER NOT NULL,
+    cantidad NUMBER(10,2),
+    id_sucursal NUMBER,
+    id_pedido NUMBER, --FK
+    PRIMARY KEY(id_venta)
+);
+ALTER TABLE venta ADD FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido);
+
+CREATE SEQUENCE secuencia_id_venta
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 99999
+MINVALUE 1;
+
+/* --------------- Relaciones --------------- */
+
+CREATE TABLE articulo_pertenece_categoria(
+    id_articulo NUMBER NOT NULL,
+    id_categoria NUMBER NOT NULL,
+    PRIMARY KEY(id_articulo, id_categoria)
+);
+ALTER TABLE articulo_pertenece_categoria ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+ALTER TABLE articulo_pertenece_categoria ADD FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria);
+
+CREATE TABLE empleado_trabaja_sucursal(
+    id_empleado NUMBER NOT NULL,
+    id_sucursal NUMBER NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_finalizacion DATE,
+    PRIMARY KEY(id_empleado, id_sucursal, fecha_inicio)
+);
+ALTER TABLE empleado_trabaja_sucursal ADD FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado);
+ALTER TABLE empleado_trabaja_sucursal ADD FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal);
+
+CREATE TABLE articulo_conforma_pedido(
+    id_pedido NUMBER NOT NULL,
+    id_articulo NUMBER NOT NULL,
+    cantidad NUMBER,
+    PRIMARY KEY(id_pedido, id_articulo)
+);
+ALTER TABLE articulo_conforma_pedido ADD FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido);
+ALTER TABLE articulo_conforma_pedido ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+
+CREATE TABLE articulo_conforma_abasto(
+    id_abasto NUMBER NOT NULL,
+    id_articulo NUMBER NOT NULL,
+    cantidad NUMBER,
+    PRIMARY KEY(id_abasto, id_articulo)
+);
+ALTER TABLE articulo_conforma_abasto ADD FOREIGN KEY (id_abasto) REFERENCES abasto(id_abasto);
+ALTER TABLE articulo_conforma_abasto ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+
+CREATE TABLE sucursal_tiene_articulo(
+    id_sucursal NUMBER NOT NULL,
+    id_articulo NUMBER NOT NULL,
+    cantidad NUMBER,
+    PRIMARY KEY(id_sucursal, id_articulo)
+);
+ALTER TABLE sucursal_tiene_articulo ADD FOREIGN KEY (id_sucursal) REFERENCES sucursal(id_sucursal);
+ALTER TABLE sucursal_tiene_articulo ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+
+CREATE TABLE bodega_guarda_articulo(
+    id_bodega NUMBER NOT NULL,
+    id_articulo NUMBER NOT NULL,
+    cantidad NUMBER,
+    PRIMARY KEY(id_bodega, id_articulo)
+);
+ALTER TABLE bodega_guarda_articulo ADD FOREIGN KEY (id_bodega) REFERENCES bodega(id_bodega);
+ALTER TABLE bodega_guarda_articulo ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+
+CREATE TABLE proveedor_provee_articulo(
+    id_proveedor NUMBER NOT NULL,
+    id_articulo NUMBER NOT NULL,
+    PRIMARY KEY(id_proveedor, id_articulo)
+);
+ALTER TABLE proveedor_provee_articulo ADD FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor);
+ALTER TABLE proveedor_provee_articulo ADD FOREIGN KEY (id_articulo) REFERENCES articulo(id_articulo);
+
+
+/* --------------- LALALAL --------------- */
