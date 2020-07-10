71630b43099505a0b84c787a9eadc9fb4da77178
mochi182 <mochi182tv@gmail.com>
2020-07-10 07:12:50 -0500

    Vista

---
+++ Vistas.sql
@@ -0,0 +1,6 @@
+/* ---------- Vistas ---------- */
+
+CREATE OR REPLACE VIEW articulos_a_la_venta AS
+    SELECT  S.id_articulo AS "ID DEL ARTICULO", A.NOMBRE 
+    FROM sucursal_tiene_articulo S, articulo A
+    WHERE A.id_articulo=S.id_articulo AND S.cantidad_actual <> 0;
